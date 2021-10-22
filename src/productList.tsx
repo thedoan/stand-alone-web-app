88fce2a91f9c46e17e9266c19564abde22467d04
thedoan <dangthedoan@gmail.com>
2021-10-22 18:08:35 +0700

    feat: using decorator metadata
     - addClass decorators

--- src/productList.tsx
+++ src/productList.tsx
@@ -2,11 +2,13 @@ import { createElement } from "./tools/jsxFactory";
 import { Product } from "./data/entities";
 import { ProductItem } from "./productItem";
 import { CategoryList } from "./categoryList";
+import { addClass } from "./decorators";
 
 export class ProductList {    
 	props: {        
 		products: Product[], categories: string[], selectedCategory: string, addToOrderCallback?: (product: Product, quantity: number) => void, filterCallback?: (category: string) => void;    
 	}    
+	@addClass("select", "bg-info", "m-1")
 	getContent(): HTMLElement {        
 		return <div className="container-fluid">            
 			<div className="row"> 
