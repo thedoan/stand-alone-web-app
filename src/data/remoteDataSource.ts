12661c836a8224a6e5483e2b2df43f7e577c22d3
thedoan <dangthedoan@gmail.com>
2021-10-22 17:26:41 +0700

    feat: add json data and web service
    - add product data in data.js

---
+++ src/data/remoteDataSource.ts
@@ -0,0 +1,21 @@
+import { AbstractDataSource } from "./abstractDataSource";
+import { Product, Order } from "./entities";
+import Axios, { AxiosResponse } from "axios";
+const protocol = "http";
+const hostname = "localhost";
+const port = 4600;
+const urls = {
+    products: `${protocol}://${hostname}:${port}/products`, orders: `${protocol}://${hostname}:${port}/orders`
+};
+export class RemoteDataSource extends AbstractDataSource {    
+    loadProducts(): Promise<Product[]> {       
+        return Axios.get<Product[]>(urls.products).then(response => response.data);    
+    }    
+    storeOrder(): Promise<number> {        
+        let orderData = {            
+            lines: [...this.order.orderLines.values()]
+                .map(ol => ({ productId: ol.product.id, productName: ol.product.name, quantity: ol.quantity }))       
+        }
+        return Axios.post<Product>(urls.orders, orderData).then(response => response.data.id);    
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { LocalDataSource } from "./data/localDataSource";
+//import { LocalDataSource } from "./data/localDataSource";
+import {RemoteDataSource} from "./data/remoteDataSource";
 import { HtmlDisplay } from "./htmlDisplay";
 import "bootstrap/dist/css/bootstrap.css";
-let ds = new LocalDataSource();
+let ds = new RemoteDataSource();
+
 function displayData(): Promise<HTMLElement> {    
 	let display = new HtmlDisplay();    
 	display.props = {        
@@ -9,6 +11,7 @@ function displayData(): Promise<HTMLElement> {
 	}    
 	return display.getContent();
 }
+
 document.onreadystatechange = () => {    
 	if (document.readyState === "complete") {        
 		displayData().then(elem => {            
